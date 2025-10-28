5efe3905bdd7ddfd30d0b3cda42f7c10ddb474cb
LAPTOP-3E4GQJH1\Phan <jatturong@gmail.com>
2025-10-28 09:24:14 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Coding Test BBL API",
+        Version = "v1",
+        Description = "A basic ASP.NET Core Web API documentation for the project."
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
